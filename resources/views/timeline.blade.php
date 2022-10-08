6bed5e9ff61ab2a046eee28f33cbb833c5433dab
fardan <fardan.nozami@gmail.com>
2022-10-08 13:48:56 +0700

    add relation many to many for follows user

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -3,7 +3,8 @@
     <div class="grid grid-cols-12 gap-6">
       <div class="col-span-7">
         <div class="space-y-6">
-          @foreach ($statuses as $status)
+          <div class="border rounded-xl p5 space-y-5">
+            @foreach ($statuses as $status)
               <div class="flex ml-6">
                 <div class="flex-shrink-0 mr-3">
                   <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/150" alt="{{ $status->user->name }}">
@@ -20,10 +21,32 @@
                   </div>
                 </div>
               </div>
-          @endforeach
+            @endforeach
+          </div>
         </div>
       </div>
-      <div class="col-span-5">Friend</div>
+      <div class="col-span-5">
+        <div class="border p-5 rounded-xl">
+          <h1 class="font-semibold mb-5">Recentlu follows</h1>
+          <div class="space-y-5">
+            @foreach (Auth::user()->follows as $user)
+              <div class="flex ml-6 items-center">
+                <div class="flex-shrink-0 mr-3">
+                  <img class="w-10 h-10 rounded-full" src="https://i.pravatar.cc/150" alt="{{ $status->user->name }}">
+                </div>
+                <div>
+                  <div class="font-semibold">
+                   {{$user->name}}
+                  </div>
+                  <div class="text-sm text-gray-600">
+                   {{ $user->pivot->created_at->diffForHumans() }}
+                  </div>
+                </div>
+              </div>
+            @endforeach
+          </div>
+        </div>
+      </div>
     </div>
   </x-container>
 </x-app-layout>
